34313630c99264ed8e9ec8241350da1d00e3d64c
paolo249 <90116237+paolo249@users.noreply.github.com>
2021-11-12 12:40:00 -0800

    Complete Step 3.2

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('items/create/', views.ItemsCreate.as_view(), name = 'item_create'),
+    path('', views.ItemList.as_view(), name='item_list'),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 from .models import Item
 
 
@@ -14,3 +15,6 @@ class ItemsCreate(CreateView):
     model = Item
     fields = '__all__'
     success_url = '/'
+
+class ItemList(ListView):
+    model = Item
