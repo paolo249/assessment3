1896ddedb297ee2516f07674bfaf890f6fe7f04a
paolo249 <90116237+paolo249@users.noreply.github.com>
2021-11-12 12:16:52 -0800

    Complete Step 3.1

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Item
 
+
+
+
+def home(request):
+  return render(request, 'home.html')
+
+
 # Create your views here.
 class ItemsCreate(CreateView): 
     model = Item
